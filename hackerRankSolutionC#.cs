69f35ad185ef6c756036b9310b65cdb744fd4174
debchy <connectdebu@gmail.com>
2022-01-14 20:19:13 +0400

    twoStrings

--- hackerRankSolutionC#.cs
+++ hackerRankSolutionC#.cs
@@ -50,6 +50,30 @@ namespace HackerRank
             Console.WriteLine( answer);
         }
 
+
+        /*twoStrings: https://www.hackerrank.com/challenges/two-strings/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
+        * Complete the 'twoStrings' function below.
+        *
+        * The function is expected to return a STRING.
+        * The function accepts following parameters:
+        *  1. STRING s1
+        *  2. STRING s2
+        */
+
+        public static string twoStrings(string s1, string s2)
+        {
+            string res="NO";
+            var aint=Convert.ToInt32('a');
+            var zint=Convert.ToInt32('z');
+            
+            for(int i=aint;i<=zint;i++){
+                char c=Convert.ToChar(i);
+                if(s1.IndexOf(c)!=-1 && s2.IndexOf(c)!=-1)return "YES";
+            }
+        
+            return res;
+        }
+
     }
 
 }
