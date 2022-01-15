c2c4a18bbcba9fbdedd9294f88e4532d537566b3
debchy <connectdebu@gmail.com>
2022-01-15 13:29:52 +0400

    sherlockAndAnagrams_2 optimized

--- hackerRankSolutionC#.cs
+++ hackerRankSolutionC#.cs
@@ -74,6 +74,86 @@ namespace HackerRank
             return res;
         }
 
+
+        /*sherlockAndAnagrams https://www.hackerrank.com/challenges/sherlock-and-anagrams/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
+        * Complete the 'sherlockAndAnagrams' function below.
+        *
+        * The function is expected to return an INTEGER.
+        * The function accepts STRING s as parameter.
+        */
+
+        public static int sherlockAndAnagrams(string s)
+        {
+            var count=0;
+            if(s.ToCharArray().Distinct().Count()==s.Count())return count;
+            
+            for(int len=1;len<s.Count();len++){
+                for(int i=0;i<s.Count()-len;i++){
+                    for(int j=i+1;j<=s.Count()-len;j++){
+                        //Console.WriteLine("=>"+s.Substring(i,len) + " , "+ s.Substring(j,len));
+                        if(len==1 && s[i]==s[j])count++;
+                        else if(isAnagram(s.Substring(i,len), s.Substring(j,len))){
+                            count++;
+                        }
+                    }
+                }
+            }
+            
+            return count;
+        }
+        
+        public static bool isAnagram(string s1,string s2){
+            var retVal=true;
+            char[] s1_a=s1.ToCharArray();
+            char[] s2_a=s2.ToCharArray();
+            IEnumerable<char> distinct=(s1+s2).ToCharArray().Distinct();
+            foreach(var e in distinct){
+                int s1c=s1_a.Count(c=>c==e);
+                int s2c=s2_a.Count(c=>c==e);
+                //Console.WriteLine(s1c+","+s2c+","+(char)i);
+                if(s1c!=s2c)return false;
+            }
+            // for(int i=(int)'a';i<=(int)'z';i++)
+            // {
+            //     int s1c=s1_a.Count(c=>c==(char)i);
+            //     int s2c=s2_a.Count(c=>c==(char)i);
+            //     //Console.WriteLine(s1c+","+s2c+","+(char)i);
+            //     if(s1c!=s2c)return false;
+            // }
+            return retVal;
+        }
+
+
+        public static int sherlockAndAnagrams_2(string s)
+        {
+            var count=0;
+            if(s.ToCharArray().Distinct().Count()==s.Count())return count;
+            
+            Dictionary<string, int> subset=new Dictionary<string, int>();
+            
+            for(int i=0;i<s.Count();i++){
+                for(int j=i;j<s.Count();j++){
+                    dynamic sub=s.Substring(i,j-i+1).ToCharArray();
+                    Array.Sort(sub);
+                    sub=new string(sub);
+                    Console.Write("=>"+sub );
+                    
+                    if(subset.ContainsKey(sub))subset[sub]++;
+                    else subset.Add(sub,1);                
+                    Console.Write("="+subset[sub] );
+                }
+            }
+            Console.WriteLine("" );
+            //Once occurrence 'o' of each frequency array is stored, 
+            //total anagrams will be the sum of o*(o-1)/2 for all different frequency arrays 
+            //because if a particular substring has 'o' anagrams in string 
+            //total o*(o-1)/2 anagram pairs can be formed.
+            foreach(var key in subset.Keys){                
+                count+=(subset[key]*(subset[key]-1))/2;
+                Console.WriteLine(key+"="+subset[key] +"; "+ (subset[key]*(subset[key]-1))/2);
+            }
+            return count;
+        }
     }
 
 }
