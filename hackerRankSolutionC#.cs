ed0737685438b73c56dd0eb2327362a651b78a9b
debchy <connectdebu@gmail.com>
2022-01-18 00:05:09 +0400

    alternatingCharacters

--- hackerRankSolutionC#.cs
+++ hackerRankSolutionC#.cs
@@ -166,7 +166,23 @@ namespace HackerRank
             return max;
         }
 
+        //remove alternating char https://www.hackerrank.com/challenges/alternating-characters/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=strings
+        //AAABBB=>AB
+        public static int alternatingCharacters(string s)
+        {
+            int count = 0;
+            for (int i = 0; i < s.Length - 1;i++)
+            {
+                if (s[i] == s[i + 1])
+                {
+                    count++;
+                    //s.Remove(i + 1, 1);
+                    //Console.WriteLine(s +" "+count);
+                }
+            }
 
+            return count;
+        }
     }
 
 
